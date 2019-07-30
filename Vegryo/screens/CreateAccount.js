f8ffda3c68534a19989cd976a71b645584e71eed
danniannie <dannielle.young@hotmail.com>
2019-07-30 16:15:19 +0100

    added handle change and started handle submit for create account

--- Vegryo/screens/CreateAccount.js
+++ Vegryo/screens/CreateAccount.js
@@ -3,58 +3,69 @@ import { StyleSheet, Text, View, Button, ScrollView, TextInput } from "react-nat
 
 export default class CreateAccount extends React.Component {
   state = {
-    Name: '', Username: '', Email: '', GardenLength: 0, GardenWidth: 0, City: ''
+    Name: '', Username: '', Email: '', GardenLength: '', GardenWidth: '', City: ''
   }
   render() {
+
     return <ScrollView >
       <Text>Create Account Page</Text>
       <View style={styles.container}>
 
         <Text>Name:</Text>
-        <TextInput placeholder="Enter your name..." value={this.state.name} style={
+        <TextInput placeholder="Enter your name..." value={this.state.Name} onChangeText={(Name) => { this.setState({ Name }) }} style={
           {
             height: 50
           }
 
         } />
         <Text>Username:</Text>
-        <TextInput placeholder="Username" value={this.state.Username} style={
+        <TextInput placeholder="Username" value={this.state.Username} onChangeText={(Username) => { this.setState({ Username }) }} style={
           {
             height: 50
           }
 
         } />
         <Text>Email:</Text>
-        <TextInput placeholder="Email" value={this.state.Email} style={
+        <TextInput placeholder="Email" value={this.state.Email} onChangeText={(Email) => { this.setState({ Email }) }} style={
           {
             height: 50
           }
 
         } />
         <Text> Garden Length:</Text>
-        <TextInput placeholder="In cm please" value={this.state.GardenLength} style={
+        <TextInput placeholder="In cm please" value={this.state.GardenLength} onChangeText={(GardenLength) => { this.setState({ GardenLength }) }} style={
           {
             height: 50
           }
 
         } />
         <Text> Garden Width:</Text>
-        <TextInput placeholder="Name" value={this.state.GardenWidth} style={
+        <TextInput placeholder="Name" value={this.state.GardenWidth} onChangeText={(GardenWidth) => { this.setState({ GardenWidth }) }} style={
           {
             height: 50
           }
 
         } />
         <Text> City:</Text>
-        <TextInput placeholder="City" value={this.state.City} style={
+        <TextInput placeholder="City" value={this.state.City} onChangeText={(City) => { this.setState({ City }) }} style={
           {
             height: 50
           }
 
         } />
-        <Button title='Submit'></Button>
+        <Button title='Submit' onPress={this.handleSubmit}></Button>
       </View>
     </ScrollView>;
+  }
+
+  handleSubmit = (event) => {
+    const { Name, Username, Email, GardenLength, GardenWidth, City } = this.state
+    const body = {
+      id: Username, text: {
+        Name, Username, Email, GardenLength, GardenWidth, City
+      }
+    }
+
   }
 
 
