00bae5580584813e3cf1b7f5635a28411d31c59c
danniannie <dannielle.young@hotmail.com>
2019-08-01 16:36:17 +0100

    updated functionality for garden

--- Vegryo/utils/utils.js
+++ Vegryo/utils/utils.js
@@ -1,19 +1,18 @@
 
 const gardenDesign = (vegetables, height, width) => {
   let area = Math.floor(height * width)
-  let sectionSpace = area / vegetables.length
+  const veggieKeys = Object.keys(vegetables)
+  let sectionSpace = area / veggieKeys.length
   let vegetableArea = []
-  vegetables.forEach(vegetable => {
-    let vegetableKey = Object.keys(vegetable)
-    let indVegetable = vegetableKey[0]
-    let vegSpacing = Math.pow(vegetable[indVegetable], 2)
-    let vegetableCount = Math.floor(sectionSpace / vegSpacing)
-    return vegetableArea.push({ [indVegetable]: vegetableCount })
-  });
 
+  veggieKeys.forEach((vegetable) => {
+    let vegSpacing = Math.pow(vegetables[vegetable], 2)
+    let vegetableCount = Math.floor(sectionSpace / vegSpacing)
+    return vegetableArea.push({ [vegetable]: vegetableCount })
+  })
+  console.log(vegetableArea, 'function output')
   return vegetableArea
 }
 
 module.exports = gardenDesign
 
- //get the garden spacing information from the user >> do we store this in the app state too?
