986ed385bf64b12d942db4359247bb3264cbb9ae
Fatimah <fatimah@Fatimahs-MacBook-Pro.local>
2019-08-01 11:52:30 +0100

    implemented section to edit each veg count

--- Vegryo/screens/MyGarden.js
+++ Vegryo/screens/MyGarden.js
@@ -1,38 +1,65 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { AuthSession } from "expo";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default class MyGarden extends React.Component {
   state = {
-    Veg: ["carrot", "broccoli", "emnkef"]
+    Veg: ["carrot", "broccoli", "potato", "asparagus"],
+    selectedVeg: ""
   };
   render() {
     const { Veg } = this.state;
+
     return (
-      <View style={styles.square}>
-        {Veg.map((veg, i) => {
-          return (
-            <View
-              style={{
-                width: "100%",
-                height: `${100 / this.state.Veg.length}%`,
-                borderColor: "red",
-                borderStyle: "solid",
-                borderWidth: 2
-              }}
-            >
-              <Text>{Veg[i]}</Text>
-            </View>
-          );
-        })}
+      <View style={{ width: "100%", height: "100%", alignItems: "center" }}>
+        <View style={styles.square}>
+          {Veg.map((veg, i) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({ selectedVeg: veg });
+                }}
+                key={veg}
+                style={{
+                  width: "100%",
+                  height: `${100 / this.state.Veg.length}%`,
+                  padding: 2
+                }}
+              >
+                <View
+                  style={{
+                    borderColor: "red",
+                    borderStyle: "solid",
+                    borderWidth: 2,
+                    height: "100%"
+                  }}
+                >
+                  <Text>{Veg[i]}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+
+        <View
+          style={{
+            width: "90%",
+            height: 80,
+            backgroundColor: "beige",
+            margin: 20
+          }}
+        >
+          <Text>{this.state.selectedVeg}</Text>
+        </View>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   square: {
+    margin: 10,
     width: 350,
-    height: 350,
+    height: 400,
     backgroundColor: "#14ff5f"
   }
 });
