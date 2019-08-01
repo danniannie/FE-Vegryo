ed5d152bc7a2ccf557a31b8a97120754e82a1056
Fatimah <fatimah@Fatimahs-MacBook-Pro.local>
2019-08-01 10:50:18 +0100

    sectioned out myGarden depending on length of veg array

--- Vegryo/screens/MyGarden.js
+++ Vegryo/screens/MyGarden.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { AuthSession } from "expo";
 
-export default function MyGarden(props) {
-  return (
-    <View>
-      <Text>My Gardennnnnnnnn</Text>
-    </View>
-  );
+export default class MyGarden extends React.Component {
+  state = {
+    Veg: ["carrot", "broccoli", "emnkef"]
+  };
+  render() {
+    const { Veg } = this.state;
+    return (
+      <View style={styles.square}>
+        {Veg.map((veg, i) => {
+          return (
+            <View
+              style={{
+                width: "100%",
+                height: `${100 / this.state.Veg.length}%`,
+                borderColor: "red",
+                borderStyle: "solid",
+                borderWidth: 2
+              }}
+            >
+              <Text>{Veg[i]}</Text>
+            </View>
+          );
+        })}
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  square: {
+    width: 350,
+    height: 350,
+    backgroundColor: "#14ff5f"
+  }
+});
