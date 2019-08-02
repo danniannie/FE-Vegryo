b03a64a5c2c220026976692fd6b0c51caa4ae64f
natasafi <natasafrag@gmail.com>
2019-08-02 10:23:26 +0100

    passes the dimension of the user;s garden

--- Vegryo/screens/Vegetables.js
+++ Vegryo/screens/Vegetables.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import VeggieCard from "../components/VeggieCard";
 import AnimatedCarrot from "../components/LoadingCard";
 import * as api from "../utils/api";
-import gardenDesign from '../utils/utils'
+import gardenDesign from "../utils/utils";
 import { update, Object } from "tcomb";
 
 export default class Vegetables extends React.Component {
@@ -13,7 +13,6 @@ export default class Vegetables extends React.Component {
     isLoading: true
   };
   render() {
-
     const { vegetables, isLoading } = this.state;
 
     return (
@@ -21,19 +20,16 @@ export default class Vegetables extends React.Component {
         {isLoading ? (
           <AnimatedCarrot />
         ) : (
-            vegetables.map((vegetable) => (
-              <VeggieCard
-                vegetable={vegetable}
-                key={vegetable.id}
-                handleAdd={this.handleAdd}
-                handleRemove={this.handleRemove}
-              />
-            ))
-          )}
-        <Button
-          title="Build your Garden"
-          onPress={this.onPress}
-        />
+          vegetables.map((vegetable) => (
+            <VeggieCard
+              vegetable={vegetable}
+              key={vegetable.id}
+              handleAdd={this.handleAdd}
+              handleRemove={this.handleRemove}
+            />
+          ))
+        )}
+        <Button title="Build your Garden" onPress={this.onPress} />
       </ScrollView>
     );
   }
@@ -45,31 +41,32 @@ export default class Vegetables extends React.Component {
   }
 
   onPress = () => {
-    const { addVegetableLayout } = this.props.screenProps
-    const vegetableLayout = gardenDesign(this.state.selectedVeggies, 100, 100)
-    addVegetableLayout(vegetableLayout)
-    this.props.navigation.navigate("MyGarden")
-  }
+    const { addVegetableLayout, height, width } = this.props.screenProps;
+    const vegetableLayout = gardenDesign(
+      this.state.selectedVeggies,
+      height,
+      width
+    );
+    addVegetableLayout(vegetableLayout);
+    this.props.navigation.navigate("MyGarden");
+  };
 
   handleAdd = (id, spacing) => {
     const { selectedVeggies } = this.state;
 
     if (!selectedVeggies[id]) {
       selectedVeggies[id] = spacing;
-      this.setState({ selectedVeggies })
+      this.setState({ selectedVeggies });
     }
-
   };
 
   handleRemove = (id) => {
     const { selectedVeggies } = this.state;
 
     if (selectedVeggies[id]) {
-      delete selectedVeggies[id]
+      delete selectedVeggies[id];
 
-      this.setState({ selectedVeggies })
+      this.setState({ selectedVeggies });
     }
-
-  }
+  };
 }
-
