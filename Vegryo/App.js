a202b5a930a062ccf199bd0189e2625c12aaf598
natasafi <natasafrag@gmail.com>
2019-08-02 15:09:57 +0100

    creates bottomNavBar with icons

--- Vegryo/App.js
+++ Vegryo/App.js
@@ -4,15 +4,10 @@ import {
   createAppContainer,
   createBottomTabNavigator
 } from "react-navigation";
-import { Icon } from "react-native-vector-icons/Ionicons";
-import Home from "./screens/Home";
-import MyGarden from "./screens/MyGarden1";
-import Vegetables from "./screens/Vegetables";
-import CreateAccount from "./screens/CreateAccount";
-import WelcomePage from "./screens/WelcomePage";
+import Icon from "react-native-vector-icons/Ionicons";
 import AppNavigator from "./Routes/AppNavigator";
 
-export default class App extends Component {
+class App extends Component {
   state = {
     vegetableLayout: [],
     height: 0,
@@ -38,38 +33,47 @@ export default class App extends Component {
   };
 }
 
-// const BottomNav = createBottomTabNavigator(
-//   {
-//     Home: { screen: appStack },
-//     MyGarden: { screen: appStack },
-//     Vegetables: { screen: appStack },
-//     CreateAccount: { screen: appStack },
-//     WelcomePage: { screen: appStack }
-//   },
-// {
-//   defaultNavigationOptions: ({ navigation }) => ({
-//     tabBarIcon: { focused, tintColor }
-//   }) => {
-//     const { routeName } = navigation.state;
-//     // let IconComponent = Ionicons;
-//     // let iconName = "";
-//     // if (routeName === "WelcomePage") {
-//     //   //if (routeName === 'MyGarden')
-//     //   //if (routeName === 'Vegetables)
-//     //   //if (routeName === 'CreateAccount')
-//     //   return <Icon name="ios-person" size={50} color="#484538" />;
-//     // }
-//   }
-// },
-// {
-//   tabBarOptions: {
-//     activeTintColor: "#ffa03a",
-//       inactiveTintColor: "#484538"
-//   }
-// }
-// );
-// //     }
-// //   }
-// // );
+const BottomNav = createBottomTabNavigator(
+  (paths = {
+    Home: {
+      screen: AppNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-home" color="#484538" size={25} />
+        )
+      }
+    },
+    Vegetables: {
+      screen: AppNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="md-leaf" color="#484538" size={25} />
+        )
+      }
+    },
+    MyGarden: {
+      screen: AppNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-flower" color="#484538" size={25} />
+        )
+      }
+    },
+    CreateAccount: {
+      screen: AppNavigator,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person" color="#484538" size={25} />
+        )
+      }
+    }
+  }),
+  {
+    tabBarOptions: {
+      activeTintColor: "#ffa03a",
+      inactiveTintColor: "#484538"
+    }
+  }
+);
 
-// export default createAppContainer(BottomNav);
+export default createAppContainer(BottomNav);
