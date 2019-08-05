cf798ffb5e140e4386b8906bfdc052a5d18daac8
janders4 <johnanderson198689@gmail.com>
2019-08-05 12:14:00 +0100

    days grown logic added to hard coded veg

--- Vegryo/components/VeggieInfo.js
+++ Vegryo/components/VeggieInfo.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import ProgressBar from "react-native-progress/Bar";
-import { getUserById } from "../utils/api.js";
+import moment from "moment";
+import { daysGrown } from "../utils/utils";
 
 export default class VeggieInfo extends Component {
   state = {
-    user: "Old McDonald"
+    user: "Old McDonald",
+    garden: ["Carrot", "Potato"],
+    plantDates: { Carrot: 1563231600, Potato: 1562153400 },
+    growTime: { Carrot: 70, Potato: 70 }
   };
   render() {
     const {
@@ -15,6 +19,7 @@ export default class VeggieInfo extends Component {
       ammountOfVeg,
       gardenWidth
     } = this.props;
+    const { plantDates, growTime } = this.state;
     return (
       <View
         style={{
@@ -34,15 +39,22 @@ export default class VeggieInfo extends Component {
             : null}
           {"\n"}
           {selectedVeg ? `Row Width: ${Math.floor(gardenWidth)}cm` : null}
+          {"\n"}
+          {selectedVeg
+            ? `Days until harvest: ${growTime[selectedVeg] -
+                Math.floor(daysGrown(plantDates[selectedVeg])) || 0} `
+            : null}
         </Text>
         {selectedVeg ? (
-          <ProgressBar progress={0.4} width={200} color={"#FFA03A"} />
+          <ProgressBar
+            progress={
+              daysGrown(plantDates[selectedVeg]) / growTime[selectedVeg] || 0
+            }
+            width={200}
+            color={"#FFA03A"}
+          />
         ) : null}
       </View>
     );
   }
-
-  componentDidMount = () => {
-    getUserById().then(user => console.log(user));
-  };
 }
