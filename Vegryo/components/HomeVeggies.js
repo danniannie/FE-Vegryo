ea1d79b70ca9a623df2560b80b0e47f4eaaba228
Fatimah <fatimah@Fatimahs-MacBook-Pro.local>
2019-08-07 09:53:33 +0100

    added the days left til harvest

--- Vegryo/components/HomeVeggies.js
+++ Vegryo/components/HomeVeggies.js
@@ -1,25 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import * as api from "../utils/api";
+import { daysGrown } from "../utils/utils";
 
 class HomeVeggies extends React.Component {
+  state = {
+    icon: []
+  };
   render() {
-    const { veg } = this.props;
+    const { icon } = this.state;
+    const { veg, date } = this.props;
+    const pic = icon[2];
     return (
       <View style={styles.veg}>
-        <Text style={{ fontSize: 18 }}>
-          {veg}: {"\n"}
+        <Image
+          style={{ width: 70, height: 70, marginRight: 7 }}
+          source={{ uri: pic }}
+          resizeMode="center"
+        />
+        <Text>
+          Your {veg}'s will be ready to harvest{"\n"} in{" "}
+          {Math.floor(daysGrown(date))} days.
         </Text>
-        <Text>Your {veg}'s will be ready to harvest in X days.</Text>
       </View>
     );
   }
+
+  componentDidMount = () => {
+    api.getAllVeggies().then(veggie => {
+      this.setState({
+        icon: veggie.map(veggie => {
+          return veggie.data.Picture;
+        })
+      });
+    });
+  };
 }
 const styles = StyleSheet.create({
   veg: {
     backgroundColor: "whitesmoke",
     margin: 10,
-    padding: 40,
-    borderRadius: 3
+    padding: 30,
+    borderRadius: 3,
+    flexDirection: "row",
+
+    alignItems: "center"
   }
 });
 
